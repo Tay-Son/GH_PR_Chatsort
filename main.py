d4f2f41fe522e83dedebc29519f3ad11de6eda14
Tay-Son <83438381+Tay-Son@users.noreply.github.com>
2022-05-24 16:11:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from sentence_transformers import SentenceTransformer
 from multiprocessing import Process, Queue
 import pytchat
 import time
+import os
+
+
 
 
 class chatsort:
@@ -12,11 +15,30 @@ class chatsort:
         self.get_chat = self.get_chat_yt
         self.freq_print = 1.0
         self.model_ = 'sentence-transformers/all-distilroberta-v1'
-        self.is_run = True
 
+        self.size_window = 1024
+
+        #Window
+        wnd_cnt = 0
+        wnd_time = [-1 for _ in range(self.size_window)]
+        wnd_uid = [-1 for _ in range(self.size_window)]
+        wnd_mes_raw = [-1 for _ in range(self.size_window)]
+        wnd_mes_ref = [-1 for _ in range(self.size_window)]
+        wnd_embed = [-1 for _ in range(self.size_window)]
+        wnd_mat = [[.0 for _ in range(self.size_window)] for _ in range(self.size_window)]
+
+
+        #Options for Embed_message
         self.opt_emb_t_l = 1.0
         self.opt_emb_s_b = 32
-        pass
+
+        #Options for Print
+        self.opt_prt_t_l = 1.0
+
+        #Options for Update_window
+        self.opt_upd_s_m = 128
+
+
 
     def refine_message(self, mew_raw):
         mes_ref = mew_raw
@@ -61,8 +83,9 @@ class chatsort:
                         temp_ = self.que_raw.get()
                         if temp_ == -1:
                             is_run = False
+                            break
                         else:
-                            time_, uid_, mes_raw, mes_ref = self.que_raw.get()
+                            time_, uid_, mes_raw, mes_ref = temp_
                             lst_time.append(time_)
                             lst_uid.append(uid_)
                             lst_mes_raw.append(mes_raw)
@@ -75,7 +98,21 @@ class chatsort:
         self.que_emb.put(-1)
         return
 
+
+
+    def print_recent_mes(self):
+        pass
+
+    def print_sort(self):
+        pass
+
     def print_(self):
+        str_output = ""
+        os.system('cls')
+        print(str_output)
+        return
+
+    def update_window(self):
         pass
 
     def run_(self):
@@ -88,7 +125,27 @@ class chatsort:
         while self.que_emb.empty(): time.sleep(.1)
 
         is_run = True
+        t_last_u = time.time()
         while is_run:
+            if self.opt_prt_t_l <= (time.time() - t_last_u):
+                self.print_()
+                t_last_u = time.time()
+            for _ in range(self.opt_upd_s_m):
+                if self.que_emb.empty():
+                    time.sleep(.1)
+                    break
+                else:
+                    temp_ = self.que_emb.get()
+                    if temp_ == -1:
+                        is_run = False
+                        break
+                    else:
+                        self.update_window()
+        proc_gc.join()
+        proc_ec.join()
+        return
+
+
 
 
 def data_source(video_id, que_origin):
